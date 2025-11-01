c0680ca7f5e412eb64a20fbd5ad386dc889664f0
banti-kumar9128 <vk9128697037@gmail.com>
2025-11-01 11:51:17 +0530

    first commit

--- src/pages/Contect/Contect.jsx
+++ src/pages/Contect/Contect.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./contec.css"
+
 
 const Contect = () => {
   return (
+
+
     <div className='contect'>
             <form action="https://formspree.io/f/xwprrboz" method='post'>
                 <h1>CONTECT ME</h1>
               <h2>Get in touch me</h2>
             <input type="text" placeholder='Enter  your name...' name='username' required />
             <input type="email" placeholder='Enter your email.' name='email' required />
+            <input type="number" placeholder='Enter your number.' name='number' required />
             <textarea name="message" placeholder='message' id="" required className='tet'></textarea>
 
             <button className='btn'>submit</button>
 
+            <p>Thank you for contacting us!</p>
 
             </form>
     
